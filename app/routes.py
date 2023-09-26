dfac2c56eaec1a59fdb4900ad77afb6322bae700
Newguinea <moshuiheiyoutiao@gmail.com>
2023-09-26 10:48:23 +0800

    add login and a script for add user

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,28 @@
 #routes.py
+from flask import render_template, flash, redirect, url_for, request
 from app import app
+from app.models import User
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.verify_password(password):
+            # log user in
+            return redirect(url_for('success'))
+        else:
+            # invalid login
+            return redirect(url_for('hello_world'))
+    else:  # Handle GET request
+        return render_template('login.html')
+@app.route('/success')
+def success():
+    return "Success"
+
+@app.route('/hello_world')
+def hello_world():
+    return "Hello World"
 
 @app.route("/")
 def home():
