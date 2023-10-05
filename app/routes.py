3834b402bfc4f2028d1d2b42ff0ad421523764df
Newguinea <moshuiheiyoutiao@gmail.com>
2023-10-05 15:14:12 +0800

    optimize the api in RESTful API standard add some validation, optimize the db

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,13 @@
 # routes.py
 from flask import render_template, flash, redirect, url_for, request
+from werkzeug.exceptions import NotFound
 from app import app
 from app.models import User
 from flask_login import login_required, LoginManager
 from flask_login import current_user, login_user, logout_user
 from app.models import Residence
 from app import db
-from flask import jsonify
+from flask import jsonify, make_response
 
 # Initialize LoginManager
 login_manager = LoginManager()
@@ -48,34 +49,34 @@ def logout():
 @login_required
 @app.route('/residences', methods=['POST'])
 def create_residence():
-    """
-    for creating the residence data in the database, data from residence_info_add
-    example request:
-    {
-    "first_name": "John",
-    "last_name": "Doe",
-    "email": "johndoe@example.com",
-    "phone_no": "123-456-7890",
-    "unit_num": 12,
-    "room_no": "A101",
-    "nfc_id": "OPTIONAL_NFC_ID"
-    }
-    """
     data = request.get_json()
-    new_residence = Residence(
-        first_name=data['first_name'],
-        last_name=data['last_name'],
-        email=data['email'],
-        phone_no=data['phone_no'],
-        unit_num=data['unit_num'],
-        room_no=data['room_no'],
-        nfc_id=data.get('nfc_id', None)  # nfc_id is optional
-    )
 
-    db.session.add(new_residence)
-    db.session.commit()
+    if not data:
+        return jsonify({"error": {"message": "Invalid input", "code": 400}}), 400
 
-    return jsonify({'message': 'Residence created successfully'}), 201
+    # Check if residence with same phone number already exists
+    existing_residence = Residence.query.filter_by(phone_no=data['phone_no']).first()
+    if existing_residence:
+        return jsonify({"error": {"message": "A residence with this phone number already exists.", "code": 400}}), 400
+
+    try:
+        new_residence = Residence(
+            first_name=data['first_name'],
+            last_name=data['last_name'],
+            email=data['email'],
+            phone_no=data['phone_no'],
+            unit_num=data['unit_num'],
+            room_no=data['room_no'],
+            nfc_id=data.get('nfc_id', None)
+        )
+
+        db.session.add(new_residence)
+        db.session.commit()
+
+        response = jsonify({'message': 'Residence created successfully'})
+        return response, 201
+    except Exception as e:
+        return jsonify({"error": {"message": "Internal server error: " + str(e), "code": 500}}), 500
 
 
 @login_required
@@ -93,29 +94,31 @@ def residence_list():
 @login_required
 @app.route('/residence_list_data')
 def residence_list_data():
-    """
-    #get name(last, firsr), room_no, email
-    return format:
-    [
-        {
-            "id": 1
-            "email": "23424251@student.uwa.edu.au",
-            "name": "Z Zehua",
-            "room_no": "222"
-        },
-        ......
-    ]
-    """
-    residences = Residence.query.all()
-    residence_list = []
-    for residence in residences:
-        residence_list.append({
-            'id': residence.id,
-            'name': residence.last_name + ' ' + residence.first_name,
-            'room_no': residence.room_no,
-            'email': residence.email
-        })
-    return jsonify(residence_list)
+    try:
+        residences = Residence.query.all()
+        residence_list = []
+
+        for residence in residences:
+            residence_data = {
+                'id': residence.id,
+                'name': f"{residence.last_name} {residence.first_name}",
+                'room_no': residence.room_no,
+                'email': residence.email
+            }
+            residence_list.append(residence_data)
+
+        return make_response(jsonify({
+            "data": residence_list,
+            "message": "Success"
+        }), 200)
+
+    except Exception as e:
+        return make_response(jsonify({
+            "error": {
+                "message": str(e),
+                "code": 500  # Internal Server Error
+            }
+        }), 500)
 
 @login_required
 @app.route('/delete_residence/<int:residence_id>', methods=['POST'])
@@ -123,10 +126,15 @@ def delete_residence(residence_id):
     """
     delete the residence with residence_id
     """
-    residence = Residence.query.get_or_404(residence_id)
-    db.session.delete(residence)
-    db.session.commit()
-    return jsonify({'message': 'Residence deleted successfully'}), 201
+    try:
+        residence = Residence.query.get_or_404(residence_id)
+        db.session.delete(residence)
+        db.session.commit()
+        return '', 204  # No Content
+    except NotFound:
+        return jsonify({"error": {"message": "Residence not found", "code": 404}}), 404
+    except Exception as e:
+        return jsonify({"error": {"message": "Internal server error: " + str(e), "code": 500}}), 500
 
 @login_required
 @app.route('/edit_residence/<int:residence_id>', methods=['POST'])
@@ -136,6 +144,12 @@ def edit_residence(residence_id):
     """
     residence = Residence.query.get_or_404(residence_id)
     data = request.get_json()
+
+    # Check for phone number uniqueness
+    existing_residence = Residence.query.filter_by(phone_no=data['phone_no']).first()
+    if existing_residence and existing_residence.id != residence_id:
+        return jsonify({"error": {"message": "A residence with this phone number already exists.", "code": 400}}), 400
+
     residence.first_name = data['first_name']
     residence.last_name = data['last_name']
     residence.email = data['email']
@@ -143,24 +157,34 @@ def edit_residence(residence_id):
     residence.unit_num = data['unit_num']
     residence.room_no = data['room_no']
     residence.nfc_id = data['nfc_id']
-    db.session.commit()
-    return jsonify({'message': 'Residence changed successfully'}), 201
+
+    try:
+        db.session.commit()
+        return jsonify({'message': 'Residence changed successfully'}), 200  # Use 200 OK for successful updates
+    except Exception as e:
+        return jsonify({"error": {"message": "Internal server error: " + str(e), "code": 500}}), 500
+
+
 @login_required
-@app.route('/residence_info_get/<int:residence_id>', methods=['GET'])
-def residence_info_get(residence_id):
+@app.route('/residences/<int:residence_id>', methods=['GET'])
+def get_residence(residence_id):
     """
     get all the information of the residence with residence_id
     """
     residence = Residence.query.get_or_404(residence_id)
     return jsonify({
-        'first_name': residence.first_name,
-        'last_name': residence.last_name,
-        'email': residence.email,
-        'phone_no': residence.phone_no,
-        'unit_num': residence.unit_num,
-        'room_no': residence.room_no,
-        'nfc_id': residence.nfc_id
-    })
+        'message': 'Success',
+        'data': {
+            'first_name': residence.first_name,
+            'last_name': residence.last_name,
+            'email': residence.email,
+            'phone_no': residence.phone_no,
+            'unit_num': residence.unit_num,
+            'room_no': residence.room_no,
+            'nfc_id': residence.nfc_id
+        }
+    }), 200
+
 
 @login_required
 @app.route('/residence/<int:residence_id>', methods=['GET'])
--- app/static/residence_list.js
+++ app/static/residence_list.js
@@ -3,45 +3,61 @@ $(document).ready(function () {
     $.ajax({
         url: '/residence_list_data',
         method: 'GET',
-        success: function (data) {
-            var table = '<table border="1"><thead><tr><th>Name</th><th>Room No</th><th>Email</th><th>Action</th></tr></thead><tbody>';
+        success: function (response) {
+            if (response.data && Array.isArray(response.data)) {
+                var table = '<table border="1"><thead><tr><th>Name</th><th>Room No</th><th>Email</th><th>Action</th></tr></thead><tbody>';
 
-            for (var i = 0; i < data.length; i++) {
-                table += '<tr id="row-' + data[i].id + '">';
-                table += '<td><a href="/residence/' + data[i].id + '" class="edit-link" data-id="' + data[i].id + '">' + data[i].name + '</a></td>';
-                table += '<td>' + data[i].room_no + '</td>';
-                table += '<td>' + data[i].email + '</td>';
-                table += '<td><button class="delete-button" data-id="' + data[i].id + '">Delete</button></td></tr>';
-            }
+                for (var i = 0; i < response.data.length; i++) {
+                    table += '<tr id="row-' + response.data[i].id + '">';
+                    table += '<td><a href="/residence/' + response.data[i].id + '" class="edit-link" data-id="' + response.data[i].id + '">' + response.data[i].name + '</a></td>';
+                    table += '<td>' + response.data[i].room_no + '</td>';
+                    table += '<td>' + response.data[i].email + '</td>';
+                    table += '<td><button class="delete-button" data-id="' + response.data[i].id + '">Delete</button></td></tr>';
+                }
 
+                table += '</tbody></table>';
+                $('#list_residence').html(table);
 
-            table += '</tbody></table>';
-            $('#list_residence').html(table);
-
-            $('.delete-button').click(function () {
-                var id = $(this).data('id');
-                $.ajax({
-                    url: '/delete_residence/' + id,
-                    method: 'POST',
-                    success: function (response) {
-                        // Remove the row from the table
-                        $('#row-' + id).remove();
-                        alert('Deleted successfully');
-                    },
-                    error: function () {
-                        alert('An error occurred');
-                    }
+                $('.delete-button').click(function () {
+                    var id = $(this).data('id');
+                    $.ajax({
+                        url: '/delete_residence/' + id,
+                        method: 'POST',
+                        success: function () {
+                            // Remove the row from the table
+                            $('#row-' + id).remove();
+                            alert('Deleted successfully');
+                        },
+                        error: function (jqXHR) {
+                            if (jqXHR.responseJSON && jqXHR.responseJSON.error && jqXHR.responseJSON.error.message) {
+                                alert('Error: ' + jqXHR.responseJSON.error.message);
+                            } else {
+                                alert('An error occurred while trying to delete the residence.');
+                            }
+                        }
+                    });
                 });
-            });
+
+            } else {
+                alert('Unexpected data format received.');
+            }
+        },
+        error: function (jqXHR) {
+            if (jqXHR.responseJSON && jqXHR.responseJSON.error && jqXHR.responseJSON.error.message) {
+                alert('Error: ' + jqXHR.responseJSON.error.message);
+            } else {
+                alert('An error occurred while fetching the residence list.');
+            }
         }
     });
 });
 
+
 // route of logout
-document.getElementById("logoutButton").addEventListener("click", function() {
-  window.location.href = "/logout";
+document.getElementById("logoutButton").addEventListener("click", function () {
+    window.location.href = "/logout";
 });
 // route to residence info add page
-document.getElementById("RegisterResidenceButton").addEventListener("click", function() {
-  window.location.href = "/residence_info_add";
+document.getElementById("RegisterResidenceButton").addEventListener("click", function () {
+    window.location.href = "/residence_info_add";
 });
