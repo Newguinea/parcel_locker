cb64f3c37ff278091901deda872a0db48f14878e
Newguinea <moshuiheiyoutiao@gmail.com>
2023-10-14 00:50:06 +0800

    will continue tomorrow

--- PiLocker.py
+++ PiLocker.py
@@ -5,7 +5,7 @@ from picamera import PiCamera
 import RPi.GPIO as GPIO
 
 from time import sleep
-
+from  hardware_connection.hardware import getLastUserCode
 
 magSensor = PiicoDev_QMC6310(range=3000)
 threshold = 100 # microTesla or 'uT'.
@@ -14,61 +14,106 @@ display = create_PiicoDev_SSD1306()
 camera = PiCamera()
 
 
+class Hardware:
+    #variables
+    def __init__(self):
+        self.door_status = False
+    # magnet detection
+    def isLockerClosed(self):
+        """
+        every 5 seconds, check if the magnet is detected
+        :param threshold:
+        :param magSensor:
+        if the magnet is detected, return True
+        else, return False
+        :return:
+        """
+        while True:
+            strength = magSensor.readMagnitude()
+            strength_str = str(strength) + ' uT'
+            print(strength_str)
+            if strength > threshold:
+                print('Strong Magnet! The locker is closed.')
+                self.door_status = True
+                return True
+            else:
+                print('The locker is open.')
+                self.door_status = False
+                return False
+            sleep(1)
 
-# magnet detection
-def isLockerClosed():
-    while True:
-        strength = magSensor.readMagnitude()
-        strength_str = str(strength) + ' uT'
-        print(strength_str)
-        if strength > threshold:
-            print('Strong Magnet! The locker is closed.')
+
+    # nfc card uid reader
+    def getUID(self):
+        """
+        read the uid of the nfc card
+        :return:
+        """
+        try:
+            uid = pn532.read_passive_target(timeout=5000)
+            uid = "".join("%02X" % i for i in uid)[:-1]
+            return uid
+        except Exception as e:
+            print(e)
+            return "00000000"
+        # TODO: why here has a KeyboardInterrupt?
+        except KeyboardInterrupt:
+            pass
+
+
+    # oled display
+    def sendToDisplay(text,line):
+        """
+        display the text on the oled screen(128x64 pixels),(4 lines, 16 characters per line)
+        :param text: the text to be displayed
+        :param line: 1-4
+        will display the text on the line of the oled screen
+        the number of line is from 1 to 8
+        """
+        # clear the screen
+        display.clear()
+        # display.fill(0)
+        display.text(text, 0, 15*(line-1), 1)
+        display.show()
+
+
+    # camera
+    def takePhoto(self):
+        try:
+            camera.capture('./image.jpg')
             return True
-        sleep(1)
+        except Exception as e:
+            print(e)
+            return False
 
 
-# nfc card uid reader
-def getUID():
-    try:
-        uid = pn532.read_passive_target(timeout=5000)
-        uid = "".join("%02X" % i for i in uid)[:-1]
-        return uid
-    except Exception as e:
-        print(e)
-        return "00000000"
-    except KeyboardInterrupt:
-        pass
+    # relay
+    def openLocker(self):
+        try:
+            GPIO.setmode(GPIO.BCM)
+            relay_pin = 18
+            GPIO.setup(relay_pin, GPIO.OUT)
+            print("Locker opened")
+            self.door_status = True
+            return True
 
+        except Exception:
+            GPIO.cleanup()
+            return False
 
-# oled display
-def sendToDisplay(text,line):
-    display.text(text, 0, 15*(line-1), 1)
-    display.show()
+        finally:
+            sleep(5)
+            GPIO.cleanup()
 
+    def pinpadCode(self):
+        """
+        TODO: get the input from the pinpad
+        :return: 4-digit pin
+        """
+        code = input("Please enter your pin: ")
+        return code
 
-# camera
-def takePhoto():
-    try:
-        camera.capture('./image.jpg')
-        return True
-    except Exception as e:
-        print(e)
-        return False
-    
-
-# relay
-def openLocker():
-    try:
-        GPIO.setmode(GPIO.BCM)
-        relay_pin = 18
-        GPIO.setup(relay_pin, GPIO.OUT)
-        print("Locker opened")
-        return True
-
-    except Exception:
-        GPIO.cleanup()
-        return False
-    
-    finally:
-        sleep(5)
-        GPIO.cleanup()
+    def pinpadMobile(self):
+        """"""
+        mobile = input("Please enter your mobile: ")
+        return mobile
--- hardware_connection/hardware.py
+++ hardware_connection/hardware.py
@@ -1,7 +1,43 @@
 import time
+from app.models import Log
 
 def print_every_5_seconds():
     """print 1 in terminal every 5 seconds"""
     while True:
         print("1")
         time.sleep(5)
+
+
+def getLastUserCode():
+    """get the data from Log table, is_taken = 0,timestamp is the latest
+    return a dict
+    {
+    code: "Success"
+    message: {
+            "code": "12345678",
+            "nfc_id": "12345678"
+        }
+    }
+    """
+
+    log = Log.query.filter_by(is_taken=0).order_by(Log.timestamp.desc()).first()
+    if log:
+        return {
+            'code': 'Success',
+            'message': {
+                'code': log.code,
+                'nfc_id': log.nfc_id
+            }
+        }
+    else:
+        return {
+            'code': 'Not_Found',
+            'message': {
+                'code': '',
+                'nfc_id': ''
+            }
+        }
+
+def preparepercelIn(Mobile):
+    """get the data from Log table, is_taken = 0,timestamp is the latest"""
+    #TODO
